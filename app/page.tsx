ec68d3d29fa647fd2957dc467618728a61c26140
Alex <alexvillafana99@gmail.com>
2025-05-07 12:54:14 -0700

    moved files around and added plaintext login

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,66 @@
-'use client'; // ensure this runs on the client side
+"use client";
 
-import { profile, printProfile } from '../data/profile';
-import styles from './page.module.css';
-import Profile from '../components/Profile'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./login.module.css";
 
-// temp test 
-console.log("Testing profile data");
-printProfile(profile);
+import React from "react";
 
-export default function Home() {
+const mockUsers: Record<string, string> = {
+  hinge: "bcd",
+  user1: "test1",
+};
+
+export default function loginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleLogin = () => {
+    const lowercaseUser = username.toLowerCase();
+    const lowercasePass = password.toLowerCase();
+    // ?. operator allows us to safely access a property or method only if the value
+    // before isnt null or undefined
+    const storedPass = mockUsers[lowercaseUser]?.toLowerCase();
+    if (storedPass && storedPass === lowercasePass) {
+      router.push("/profile");
+    } else {
+      setError("Invalid username or password");
+    }
+  };
+
   return (
-    <main className={styles.page}>
-      <Profile
-        name={profile.name}
-        height={profile.height}
-        age={profile.age}
-        location={profile.location}
-        photos={profile.photos}
-        prompts={profile.prompts}
-        // add more props here as needed
-      />
+    <main>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Login</h1>
+        {/* username */}
+        <input
+          type="text"
+          placeholder="Where we matched .."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className={styles.input}
+        />
+
+        {/* password */}
+        <input
+          type="text"
+          placeholder="Building we met infront of ..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className={styles.input}
+        />
+
+        {/* login button */}
+        <button className={styles.button} onClick={handleLogin}>
+          Log In
+        </button>
+
+        {/* error message */}
+        {error && <p className={styles.error}>{error}</p>}
+      </div>
     </main>
   );
 }
