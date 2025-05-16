0ac43220d5f649f322050f9c7f73854c20c62fe7
Alex <alexvillafana99@gmail.com>
2025-05-16 05:27:00 -0700

    adkasdf

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -28,11 +28,11 @@ function SuccessContent() {
 
   const flipCards = [
     {
-      image: "/images/biting.JPG",
+      image: "/images/biting.jpg",
       text: "You know I hate photos but for you, I will always take them with you ... well, 9/10 times ... depends how much sleep I have had.",
     },
     {
-      image: "/images/kissing.JPG",
+      image: "/images/kissing.jpg",
       text: "The image didnt size down well so this is a selfie of you I guess haha. I get to show you off on the web now ..",
     },
     {
